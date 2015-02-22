d78790e2822711090cd05f364af8d41bd7f45edb
airrick213 <erickim213@gmail.com>
2015-02-22 11:28:10 -0500

    added fade to load page

--- EmojiChat/ViewController.swift
+++ EmojiChat/ViewController.swift
@@ -6,14 +6,21 @@
 //  Copyright (c) 2015 Eric Kim. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loadpage: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        UIView.animateWithDuration(3, animations: {
+            self.loadpage.alpha = 0
+            }, completion: { finished in
+                self.loadpage.hidden = true
+        })
     }
 
     override func didReceiveMemoryWarning() {
