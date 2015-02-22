7e9a88f31619f8f6f7c5ec76ca9ec29c5f7095c7
airrick213 <erickim213@gmail.com>
2015-02-22 17:26:18 -0500

    formatted chatroom

--- EmojiChat/ChatRoom.swift
+++ EmojiChat/ChatRoom.swift
@@ -16,5 +16,14 @@ class ChatRoom: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         nameLabel.text = nameString
+        
+        let button   = UIButton.buttonWithType(UIButtonType.System) as UIButton
+        button.frame = CGRectMake(255, 315, 100, 50)
+        button.backgroundColor = UIColor(red: 0.23, green: 0.6, blue: 0.9, alpha: 1.0)
+        button.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        button.setTitle("Send", forState: UIControlState.Normal)
+        button.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.view.addSubview(button)
     }
 }
