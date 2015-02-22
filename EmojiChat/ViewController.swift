7e913d0ff158977d042869469565e2cf12f5518c
airrick213 <erickim213@gmail.com>
2015-02-22 14:24:17 -0500

    created contact list

--- EmojiChat/ViewController.swift
+++ EmojiChat/ViewController.swift
@@ -8,21 +8,100 @@
 
 import Foundation
 import UIKit
+import AddressBook
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!    
+    var textArray: NSMutableArray! =  NSMutableArray()
+    var addressBook : ABAddressBook!
+    let people :[ABRecord] = []
 
-    @IBOutlet weak var loadpage: UIView!
+    
+    func createAddressBook() -> Bool {
+        if self.addressBook != nil {
+            return true
+        }
+        var err : Unmanaged<CFError>? = nil
+        let addressBook : ABAddressBook? = ABAddressBookCreateWithOptions(nil, &err).takeRetainedValue()
+        if addressBook == nil {
+            println(err)
+            self.addressBook = nil
+            return false
+        }
+        self.addressBook = addressBook
+        return true
+    }
+    
+    func determineStatus() -> Bool {
+        let status = ABAddressBookGetAuthorizationStatus()
+        switch status {
+        case .Authorized:
+            return self.createAddressBook()
+        case .NotDetermined:
+            var ok = false
+            ABAddressBookRequestAccessWithCompletion(nil) {
+                (granted:Bool, err:CFError!) in
+                dispatch_async(dispatch_get_main_queue()) {
+                    if granted {
+                        ok = self.createAddressBook()
+                    }
+                }
+            }
+        case .Restricted:
+            self.addressBook = nil
+            return false
+        case .Denied:
+            self.addressBook = nil
+            return false
+        }
+        return false
+    }
+    
+    
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        UIView.animateWithDuration(3, animations: {
-            self.loadpage.alpha = 0
-            }, completion: { finished in
-                self.loadpage.hidden = true
-        })
+        
+        self.createAddressBook()
+        if self.determineStatus() {
+            let people = ABAddressBookCopyArrayOfAllPeople(addressBook).takeRetainedValue() as NSArray as [ABRecord]
+            for person in people {
+                self.textArray.addObject(ABRecordCopyCompositeName(person).takeRetainedValue())
+            }
+        }
+        
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 44.0
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.textArray.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel?.text = self.textArray.objectAtIndex(indexPath.row) as? String
+        
+        return cell
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
